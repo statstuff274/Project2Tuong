387b7f4521bffd4aa0497f212d5cdcc13b139836
John Tuong <ybjaytea@Johns-MacBook-Pro-3.local>
2024-11-08 16:48:15 -0600

    Eighth Commit.

--- Project2TuongShiny/app.R
+++ Project2TuongShiny/app.R
@@ -14,6 +14,7 @@
 #install.packages("bslib")
 #install.packages("ggplot2")
 #install.packages("stringr")
+# <- install.packages("DT")
 
 library(shiny)
 library(shinyalert)
@@ -22,6 +23,7 @@ library(tidyverse)
 library(bslib)
 library(ggplot2)
 library(stringr)
+library(DT)
 
 # Time for application creation. I will be using the base app.R template and a bit of the template from my first Shiny App project to reference.
 
@@ -54,11 +56,6 @@ ui <- fluidPage(
                            choices = c("All", mobdevdata$Gender),
                            options = NULL,
                            width = NULL),
-            selectizeInput(inputId = "user_behavior_class",
-                           label = "Select User Behavior Classification:",
-                           choices = c("All", mobdevdata$User.Behavior.Class),
-                           options = NULL,
-                           width = NULL),
           
           # Created header for selecting the first numeric variable
           h2("Select Your First Numeric Variable"),  
@@ -66,13 +63,13 @@ ui <- fluidPage(
             # Created selecticized inputs w/ internal IDs for numeric variables - use corr_n method to reference the index*
             selectizeInput(inputId = "num_var1",
                            label = "Select your first numeric variable:",
-                           choices = c("Screen.on.Time..hours.day.", "Battery.Drain..mAh.day.", "Data.Usage..MB.day."),
+                           choices = numeric_vars,
                            options = NULL,
                            width = NULL),
             
-            sliderInput(inputId = "screen_time",
-                        label = c("Choose amount of Battery Drain \n (mAh per day):"),
-                        min = 300,
+            sliderInput(inputId = "value_range1",
+                        label = NULL,
+                        min = 0,
                         max = 3000,
                         value = 30),
           
@@ -80,17 +77,21 @@ ui <- fluidPage(
           h2("Select Your Second Numeric Variable"),
           
           # Created selecticized inputs w/ internal IDs for numeric variables - use corr_n method to reference the index*
-          selectizeInput(inputId = "num_var1",
+          selectizeInput(inputId = "num_var2",
                          label = "Select your first numeric variable:",
-                         choices = c("Screen.on.Time..hours.day.", "Battery.Drain..mAh.day.", "Data.Usage..MB.day."),
+                         choices = numeric_vars,
                          options = NULL,
                          width = NULL),
           
-          sliderInput(inputId = "screen_time",
-                      label = c("Choose amount of Battery Drain \n (mAh per day):"),
-                      min = 300,
+          sliderInput(inputId = "value_range2",
+                      label = NULL,
+                      min = 0,
                       max = 3000,
-                      value = 30)
+                      value = 30),
+          
+          # Created action button to update the analysis based on subset
+          actionButton(inputId = "update",
+                       label = "Update Analysis")
         ),
 
         # Created main panel
@@ -101,19 +102,43 @@ ui <- fluidPage(
                      p("The purpose of this application is to observe mobile device usage across different variables to analyze mobile device behavior."),
                      h3("Available Data to Choose from:"),
                      p("- You are able to subset information about mobile device usage including device information, usage metrics, user demographics, and user behavioral classifications."),
-                     p("- You can find the following mobile device usage data set by coping and pasting this link in your: https://www.kaggle.com/datasets/valakhorasani/mobile-device-usage-and-user-behavior-dataset?resource=download.")
+                     p("- You can find the following mobile device usage data set by coping and pasting this link in your: https://www.kaggle.com/datasets/valakhorasani/mobile-device-usage-and-user-behavior-dataset?resource=download."),
+                     h3("Notes:"),
+                     p("Data Download: this tab at the top allows you view the subsetted data and download it asf a .csv file."),
+                     p("Data Exploration: this tab at the top allows you to observe numerical and graphical summaries of your subsetted data.")
                      ),
-            tabPanel("Data Download", "placeholder"),
-            tabPanel("Data Exploration", "placeholder")
-          ), 
-          plotOutput("distPlot"),
+            tabPanel("Data Download",
+                     DT::dataTableOutput("data_table"),
+                     downloadButton(outputId = "download_data",
+                                    label = "Download Data")),
+            tabPanel("Data Exploration", 
+                     h3("Explore your subsetted data!"),
+                     plotOutput("scatterplot"))
+          )
         )
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+# Define the server for inputs and outputs
+# Steps: Inputs to subset and filter (reactive data table), Outputs (rendering data table, download .csv, )
+server <- function(input, output, session) {
+  
+    # Input - Creating subsetted/filtered data for numeric variable selection
 
+
+  
+    # OutPut - Download handler for the filtered data as a csv. file. for button in tab 2
+    output$download_data <- downloadHandler(
+      filename = function() {
+        paste('data', Sys.Date(), '.csv', sep ='')
+      },
+      content = function(con) {
+        write.csv(data, con)
+      }
+    )
+    
+    
+    
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
