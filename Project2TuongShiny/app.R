de7cf894c95da37d9a795533cbe4aaab6375d0bf
John Tuong <ybjaytea@Johns-MacBook-Pro-3.local>
2024-11-08 20:13:12 -0600

    Commit.

--- Project2TuongShiny/app.R
+++ Project2TuongShiny/app.R
@@ -38,7 +38,7 @@ ui <- fluidPage(
         sidebarPanel(
           
           # Created header for selecting categorical variables
-          h2("Select Categorical Variables \n to Subset the Data:"),
+          h3("Select Categorical Variables \n to Subset the Data:"),
             
             # Created selecticized inputs here w/ internal IDs for categorical variables
             selectizeInput(inputId = "device_model",
@@ -58,7 +58,7 @@ ui <- fluidPage(
                            width = NULL),
           
           # Created header for selecting the first numeric variable
-          h2("Select Your First Numeric Variable"),  
+          h3("Select Your First Numeric Variable"),  
             
             # Created selecticized inputs w/ internal IDs for numeric variables - use corr_n method to reference the index*
             selectizeInput(inputId = "num_var1",
@@ -71,12 +71,12 @@ ui <- fluidPage(
                         label = NULL,
                         min = 0,
                         max = 3000,
-                        value = 30),
+                        value = c(0, 3000)),
           
           # Created header for selecting the second numeric variable
           h2("Select Your Second Numeric Variable"),
           
-          # Created selecticized inputs w/ internal IDs for numeric variables - use corr_n method to reference the index*
+          # Created selecticized inputs w/ internal IDs for numeric variables and their sliders
           selectizeInput(inputId = "num_var2",
                          label = "Select your first numeric variable:",
                          choices = numeric_vars,
@@ -87,14 +87,14 @@ ui <- fluidPage(
                       label = NULL,
                       min = 0,
                       max = 3000,
-                      value = 30),
+                      value = c(0, 3000)),
           
           # Created action button to update the analysis based on subset
           actionButton(inputId = "update",
                        label = "Update Analysis")
         ),
 
-        # Created main panel
+        # Created main panel and other panels
         mainPanel(
           tabsetPanel(
             tabPanel("About",
@@ -104,8 +104,8 @@ ui <- fluidPage(
                      p("- You are able to subset information about mobile device usage including device information, usage metrics, user demographics, and user behavioral classifications."),
                      p("- You can find the following mobile device usage data set by coping and pasting this link in your: https://www.kaggle.com/datasets/valakhorasani/mobile-device-usage-and-user-behavior-dataset?resource=download."),
                      h3("Notes:"),
-                     p("Data Download: this tab at the top allows you view the subsetted data and download it asf a .csv file."),
-                     p("Data Exploration: this tab at the top allows you to observe numerical and graphical summaries of your subsetted data.")
+                     p("Data Download: this tab at the top allows you view the subsetted data and download it as a .csv file."),
+                     p("Data Exploration: this tab at the top  allows you to observe numerical and graphical summaries of your subsetted data.")
                      ),
             tabPanel("Data Download",
                      DT::dataTableOutput("data_table"),
@@ -123,31 +123,62 @@ ui <- fluidPage(
 # Steps: Inputs to subset and filter (reactive data table), Outputs (rendering data table, download .csv, )
 server <- function(input, output, session) {
   
+    # Input - Updating input boxes so the same numeric variable cannot be chosen
+    observeEvent(c(input$num_var1, input$num_var2), {
+      num_var1 <- input$num_var1
+      num_var2 <- input$num_var2
+      choices <- numeric_vars
+      if (num_var1 == num_var2) {
+        choices <- choices[-which(choices == num_var1)]
+        updateSelectizeInput(session,
+                             "num_var2",
+                             choices = choices)
+      }
+    })
+  
     # Input - Creating subsetted/filtered data for numeric variable selection
+    # Using a reactive() to pass subsequent values by if statements using req() to isolate the reactive expression when checking preconditions.
+    filtered_data <- reactive({
+      req(input$update)
+      subsetted_data <- mobdevdata 
+      if (input$device_model != "All") {
+        subsetted_data <- subsetted_data[subsetted_data$Device.Model == input$device_model]
+      }
+      if (input$operating_system != "All") {
+        subsetted_data <- subsetted_data[subsetted_data$Operating.System == input$operating_system]
+      }
+      if (input$gend != "All") {
+        subsetted_data <- subsetted_data[subsetted_data$Gender == input$gend]
+      }
+      if (input$num_var1 %in% colnames(subsetted_data)) {
+        subsetted_data <- subsetted_data[subsetted_data$num_var1 >= input$value_range1[1] & subsetted_data$num_var1 <= input$value_range1[2]]
+      }
+      if (input$num_var2 %in% colnames(subsetted_data)) {
+        subsetted_data <- subsetted_data[subsetted_data$num_var2 >= input$value_range1[1] & subsetted_data$num_var2 <= input$value_range2[2]]
+      }      
+      subsetted_data
+    })
 
-
+    
+    # OutPut - Rendering the data table for tab 2 from filtered_data subset above
+    output$data_table <- DT::renderDataTable({
+      filtered_data()
+    })
   
     # OutPut - Download handler for the filtered data as a csv. file. for button in tab 2
     output$download_data <- downloadHandler(
       filename = function() {
-        paste('data', Sys.Date(), '.csv', sep ='')
+        paste('filtered_data', Sys.Date(), '.csv', sep ='')
       },
       content = function(con) {
-        write.csv(data, con)
+        write.csv(filtered_data, con)
       }
     )
     
-    
-    
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    # OutPut - Preventing the from returning a plot the first time before Update as been clicked
+    output$scatterplot <- renderPlot({
+      if (input$update == 0)
+        return()
     })
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
+---
+editor: 
+  markdown: 
+    wrap: sentence
+---
+
 # Project2Tuong
-Project 2 - Shiny (Mobile Device Usage)
+
+## Project 2 - Shiny (Mobile Device Usage).
+
+This project contains two important files: Project2Tuong.qmd and app.R in Project2TuongShiny, and was created using RStudio.
+
+### Preparing the Data and Exploratory Analysis - .qmd
+
+-   The .qmd file contains the data set to prepare for the mobile device application and general exploratory analysis of the mobile device usage data set; here you will find contingency tables, numerical summaries, and different plots analyzing different variables regarding mobile device usage that I have found interesting and explored.
+-   Feel free to check it out on GitHub! Link: https://statstuff274.github.io/Project2Tuong/Project2Tuong.html
+
+### Mobile Device Analysis Application - R Shiny
+
+The app.R is a mobile device analyzation application allowing users to subset the data set by categorical and numeric variables according to render, view, and download filtered data sets as well as provides numerical and graphical visualizations to explore the filtered data.
+
+\- Application Instructions:
+
+> 1\.
+> Choose a specific level from each of the three categorical variables (model device, operating system, and gender) - if nothing is chosen, All levels for each variable will be applied.
+
+> 2\.
+> Choose a range of values from the two numerical values to subset (there are three numerical variables you may choose from: battery drain, data usage, and screen time)
+
+> 3\.
+> Click Update Analysis to subset the data accordingly then sift through the Data Download and Data Exploration tab to view your filtered data.
+
+> 4\.
+> The Data Download tab will allow you to view your filtered results and download that as a .csv file.
+
+> 5\.
+> The Data Exploration tab will allow you to observe numerical and graphical summaries based on your filtered data.
+
+Thank you for coming by! Feel free to contact me @jhtuong@ncsu.edu.
+
+### Quick Reference/Links
+
+Deployed Links: - GitHub qmd: https://statstuff274.github.io/Project2Tuong/Project2Tuong.html
